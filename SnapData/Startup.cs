44706e6550eec38e1bb5d440235cc0bf200b4454
Exel Gamboa <egamboa@bcposgroup.com>
2019-12-09 17:04:23 -0400

    Initial Project Commit

---
+++ SnapData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SnapData.Startup))]
+namespace SnapData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
